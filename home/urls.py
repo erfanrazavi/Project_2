5af539583c106c3858638083802f580f9ee81b30
Erfan Razavi <erfan6235@gmail.com>
2024-08-18 06:52:15 +0330

    change html and css files
    add -extends
    add -page link
    -change css style

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,12 @@ from .views import *
 
 app_name = 'index'
 urlpatterns = [
-    path('' , home_views , name = 'index')
+    path('' , home_views , name = 'index'),
+    path('about' , about_views , name='about'),
+    path('take_action' , take_action_views , name='take_action'),
+    path('news' , news_views , name='news'),
+    path('doctores' , doctores_views , name='doctores'),
+    path('contact' , contact_views , name='contact')
 ]
 
 
--- home/views.py
+++ home/views.py
@@ -5,3 +5,18 @@ from django.shortcuts import render
 # Create your views here.
 def home_views(request):
     return render(request , 'home/index.html')
+
+def about_views(request):
+    return render(request , 'home/about.html')
+
+def take_action_views(request):
+    return render(request , 'home/action.html')
+
+def news_views(request):
+    return render(request , 'home/news.html')
+
+def doctores_views(request):
+    return render(request , 'home/doctores.html')
+
+def contact_views(request):
+    return render(request , 'home/contact.html')
